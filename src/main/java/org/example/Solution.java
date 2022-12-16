023856881ff8ab64e46c5270a6389d0fe59e2e70
annt <annt@gft.com>
2022-12-16 11:00:59 +0000

    Done

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,25 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int minCostToMoveChips(int[] position) {
+
+        int even = 0, odd = 0;
+
+        for (int i : position) {
+
+            if (i % 2 == 0){
+
+                even++;
+            } else{
+
+                odd++;
+            }
+        }
+
+        return Math.min(odd, even);
     }
 }
--- src/test/java/com/example/SolutionTest.java
+++ src/test/java/com/example/SolutionTest.java
@@ -1,2 +1,48 @@
-package com.example;public class SolutionTest {
+package com.example;
+
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int[] position;
+    int result;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void twoPositionsTwoChips(){
+
+        position = new int[] {1,2};
+        result = 1;
+
+        assertEquals(result, solution.minCostToMoveChips(position));
+    }
+
+    @Test
+    public void twoPositionsFourChips(){
+
+        position = new int[] {1,2,2,2};
+        result = 1;
+
+        assertEquals(result, solution.minCostToMoveChips(position));
+    }
+
+    @Test
+    public void fivePositionsFiveChips(){
+
+        position = new int[] {1,2,3,4,5};
+        result = 2;
+
+        assertEquals(result, solution.minCostToMoveChips(position));
+    }
 }
